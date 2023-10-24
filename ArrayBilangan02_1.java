98d05ead4f87d9dabbc4f9d2bc3713f15eeafaf1
aidatulrosida <144088067+aidatulrosida@users.noreply.github.com>
2023-10-24 13:54:30 +0700

    Add files via upload

---
+++ ArrayBilangan02_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+   
+public class ArrayBilangan02_1 {
+  public static void main(String args[]){  
+    int[] bil = {5, 13, -7, 17};
+
+    for (int i : bil ){
+        System.out.println(i);
+    }
+  }
+}
